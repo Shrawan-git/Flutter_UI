54d33cfbbed5f7d06e607bf5e06391b209d84b86
Shrawan-git <shrawanshaky3@gmail.com>
2020-11-12 09:10:46 +0545

    Dashboard design

--- lib/home.dart
+++ lib/home.dart
@@ -25,7 +25,7 @@ class MainContent extends StatelessWidget {
     return new ListView(
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
           child: new Container(
             child: new Column(
               children: <Widget>[
@@ -38,47 +38,162 @@ class MainContent extends StatelessWidget {
                     ),
                   ],
                 ),
+                new SizedBox(
+                  height: 10.0,
+                ),
                 Row(
                   children: <Widget>[
                     new Expanded(
-                        child: new Container(
-                      height: 100.0,
-                      decoration: new BoxDecoration(
-                          borderRadius: new BorderRadius.circular(5.0),
-                          color: Colors.red),
-                      child: new Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          new Icon(
-                            Icons.drive_eta,
-                            color: Colors.white,
-                          ),
-                          new Text("Motor",
-                              style: new TextStyle(color: Colors.white))
-                        ],
+                        child: Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: new Container(
+                        height: 100.0,
+                        decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(5.0),
+                            color: Colors.red),
+                        child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Icon(
+                              Icons.drive_eta,
+                              color: Colors.white,
+                            ),
+                            new Text("Motor",
+                                style: new TextStyle(color: Colors.white))
+                          ],
+                        ),
                       ),
                     )),
                     new Expanded(
-                        child: new Container(
-                      height: 100.0,
-                      child: Column(
-                        children: <Widget>[
-                          Expanded(
-                            child: new Container(
-                              color: Colors.redAccent,
-                            ),
-                          ),
-                          Expanded(
-                            child: new Container(
-                              color: Colors.purpleAccent,
-                            ),
-                          )
-                        ],
-                      ),
+                        child: Padding(
+                      padding: const EdgeInsets.only(bottom: 2.5, right: 2.5),
+                      child: new Container(
+                          height: 100.0,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                  child: new Container(
+                                decoration: new BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                                child: new Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(right: 8.0),
+                                      child: new Icon(
+                                        Icons.local_offer,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    new Text(
+                                      "Classified",
+                                      style: new TextStyle(color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              )),
+                              Expanded(
+                                  child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 5, right: 1.5),
+                                child: new Container(
+                                  decoration: new BoxDecoration(
+                                      color: Colors.orangeAccent,
+                                      borderRadius:
+                                          new BorderRadius.circular(5.0)),
+                                  child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: new Icon(
+                                          Icons.beenhere,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      new Text(
+                                        "Service",
+                                        style:
+                                            new TextStyle(color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )),
+                            ],
+                          )),
                     )),
                     new Expanded(
-                        child: new Container(
-                      height: 100.0,
+                        child: Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: new Container(
+                          height: 100.0,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 2.5, bottom: 2.5),
+                                child: new Container(
+                                  decoration: new BoxDecoration(
+                                      color: Colors.lightGreen,
+                                      borderRadius:
+                                          new BorderRadius.circular(5.0)),
+                                  child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: new Icon(
+                                          Icons.account_balance,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      new Text(
+                                        "Properties",
+                                        style:
+                                            new TextStyle(color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )),
+                              Expanded(
+                                  child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 1.5, top: 2.5),
+                                child: new Container(
+                                  decoration: new BoxDecoration(
+                                      color: Colors.purple,
+                                      borderRadius:
+                                          new BorderRadius.circular(5.0)),
+                                  child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: new Icon(
+                                          Icons.art_track,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      new Text(
+                                        "Jobs",
+                                        style:
+                                            new TextStyle(color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )),
+                            ],
+                          )),
                     )),
                   ],
                 )
